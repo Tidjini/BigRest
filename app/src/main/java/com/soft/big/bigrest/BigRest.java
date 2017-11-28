af4ad84d03d82850dec94e5252a7a6a13fe3b58e
Tidjini <tidjini.messaoudi@gmail.com>
2017-11-28 05:49:24 +0100

    #v1.0.0.2 test

--- app/src/main/java/com/soft/big/bigrest/BigRest.java
+++ app/src/main/java/com/soft/big/bigrest/BigRest.java
@@ -3,11 +3,15 @@ package com.soft.big.bigrest;
 import android.app.Application;
 
 import com.soft.big.bigrest.Behaviors.TypefaceUtil;
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+import com.microsoft.appcenter.push.Push;
 
 /**
  * Created by Tidjini on 10/11/2017.
  *
- * TODO get product family
+ * TODO get product family with spinner and filter adapter
  */
 
 //Manifest file refer this application class
@@ -21,5 +25,8 @@ public class BigRest extends Application {
         // font from assets: "assets/fonts/Roboto-Regular.ttf
         TypefaceUtil.overrideFont(getApplicationContext(), "SERIF", "fonts/Exo-Light.ttf");
 
+        //code-push
+        AppCenter.start(this, "62a530f3-a55e-42a4-a4c8-4d6bd5c615c0",
+                Analytics.class, Crashes.class, Push.class);
     }
 }
