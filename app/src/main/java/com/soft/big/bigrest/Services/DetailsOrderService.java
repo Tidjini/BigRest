b26a073894996fed5fb9af3c1375fcb5d6b555a2
Tijini <tidjini.messaoudi@outlook.fr>
2019-02-26 09:36:22 +0100

    #insert details bon with success

--- app/src/main/java/com/soft/big/bigrest/Services/DetailsOrderService.java
+++ app/src/main/java/com/soft/big/bigrest/Services/DetailsOrderService.java
@@ -48,18 +48,7 @@ public class DetailsOrderService {
                 "Id = ?";
     }
 
-    private static String createDetailsOrderQueryBuilder(){
-        return "INSERT INTO "+ DETAILS_ORDER_TABLENAME+" " +
-                "(Id, DateModification, DateCreation, CreerPar, IdProduit, IdBon, ProduitDesignation, TvaProduit,"+
-                " PrixVProduit, Qte, QteEnStock, Remise, Supplement, MtTotal, MtSupplement, Imprimante, IsPrinted, Modified) " +
-                " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-        /*return "INSERT INTO "+ DETAILS_ORDER_TABLENAME+" " +
-                "(NumCmd, CodeProd, LibeProd,"+
-                " TypPrd, Idmag) " +
-                " VALUES (?, ?, ?, ?)";*/
-
-    }
 
     private static String updateDetailsOrderQueryBuilder(){
         return "UPDATE "+DETAILS_ORDER_TABLENAME+"\n" +
@@ -152,6 +141,21 @@ public class DetailsOrderService {
     }
 
 
+
+
+
+    private static String createDetailsOrderQueryBuilder(){
+        return "INSERT INTO "+ DETAILS_ORDER_TABLENAME+" " +
+                "(DateModification, DateCreation, CreerPar, IdProduit, IdBon, ProduitDesignation, TvaProduit,"+
+                " PrixVProduit, Qte, QteEnStock, Remise, Supplement, MtTotal, MtSupplement, Imprimante, IsPrinted, Modified) " +
+                " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )";
+
+        /*return "INSERT INTO "+ DETAILS_ORDER_TABLENAME+" " +
+                "(NumCmd, CodeProd, LibeProd,"+
+                " TypPrd, Idmag) " +
+                " VALUES (?, ?, ?, ?)";*/
+
+    }
     /**
      * create Detail order
      * when click on order button we fetch all details and create them using this method
@@ -159,7 +163,6 @@ public class DetailsOrderService {
      * @param detailsOrder
      * @return
      */
-
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     public static int createDetailsOrder(Connection connection, DetailsOrder detailsOrder){
 
@@ -172,25 +175,25 @@ public class DetailsOrderService {
                     createDetailsOrderQueryBuilder()
                     ,
                     Statement.RETURN_GENERATED_KEYS);
-            statement.setInt(1, detailsOrder.getNbrLigne());
+            //statement.setInt(1, detailsOrder.getNbrLigne());
+            statement.setDate(1, new Date(System.currentTimeMillis()));
             statement.setDate(2, new Date(System.currentTimeMillis()));
-            statement.setDate(3, new Date(System.currentTimeMillis()));
             //set user id
-            statement.setInt(4, 0);
-            statement.setInt(5, detailsOrder.getCodeProd());
-            statement.setInt(6, detailsOrder.getNumCmd());
-            statement.setString(7, detailsOrder.getLibeProd());
-            statement.setBigDecimal(8, detailsOrder.getTvaArt());
-            statement.setBigDecimal(9, detailsOrder.getPrixProd());
+            statement.setInt(3, 0);
+            statement.setInt(4, detailsOrder.getCodeProd());
+            statement.setInt(5, detailsOrder.getNumCmd());
+            statement.setString(6, detailsOrder.getLibeProd());
+            statement.setBigDecimal(7, detailsOrder.getTvaArt());
+            statement.setBigDecimal(8, detailsOrder.getPrixProd());
+            statement.setBigDecimal(9, detailsOrder.getQttProd());
             statement.setBigDecimal(10, detailsOrder.getQttProd());
-            statement.setBigDecimal(11, detailsOrder.getQttProd());
-            statement.setBigDecimal(12, detailsOrder.getRemArt());
-            statement.setString(13, detailsOrder.getLibeProd());
-            statement.setBigDecimal(14, detailsOrder.getMtnetArt());
-            statement.setBigDecimal(15, new BigDecimal(0));
-            statement.setString(16, detailsOrder.getLibeProd());
+            statement.setBigDecimal(11, detailsOrder.getRemArt());
+            statement.setString(12, "Supplement");
+            statement.setBigDecimal(13, detailsOrder.getMtnetArt());
+            statement.setBigDecimal(14, new BigDecimal(0));
+            statement.setString(15, "Imprimante");
+            statement.setBoolean(16, false);
             statement.setBoolean(17, false);
-            statement.setBoolean(18, false);
 
             int affectedRows = statement.executeUpdate();
             if (affectedRows == 0) {
@@ -206,6 +209,7 @@ public class DetailsOrderService {
                 }
             }
         } catch (SQLException e) {
+            Log.e(TAG, e.getMessage());
             return 0;
         }
 
